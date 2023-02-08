693ad9f5d34af3a3a0d69a520af725e1213ad183
masa <amtgptwmjgad@yahoo.co.jp>
2023-02-08 07:56:06 +0000

    validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(name: "gal",email: "galgal@yahoo.co.jp")
+    @user = User.new(name: "gal",email: "galgal@yahoo.co.jp",password: "unkounko",password_confirmation: "unkounko")
   end
   
    test "should be valid" do
@@ -36,8 +36,8 @@ class UserTest < ActiveSupport::TestCase
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
-    
   end 
+  
     
     test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
@@ -46,16 +46,22 @@ class UserTest < ActiveSupport::TestCase
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
-  end
+  end 
   
   test 'email addresses should be unique' do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save 
     assert_not duplicate_user.valid?
   end
-end 
-
-
-
-
+  
+  
+  test 'password shouldnt be empty' do 
+    @user.password =@user.password_confirmation = " " * 6
+    assert_not @user.valid? 
+  
+  end 
+  test 'password should have a minimum length' do 
+    @user.password =@user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+end
+end
