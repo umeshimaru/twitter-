4d60b8195b68c52c3cc79a9b920523ddd389da4d
Ubuntu <ubuntu@ip-172-31-32-179.ap-northeast-1.compute.internal>
2023-02-22 13:24:23 +0000

    remember me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  
+  attr_accessor :remember_token
   before_save {self.email = self.email.downcase }
 validates :name,  presence: true,length: { maximum: 100 }
 
@@ -16,4 +18,27 @@ validates :password, presence: true, length: { minimum: 6 }
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-end
+  
+  #ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember 
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest,User.digest(remember_token))
+  end
+  
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  #ユーザーのログイン情報を破棄する
+  def forget 
+    update_attribute(:remember_digest,nil)
+  end 
+    
+  end
