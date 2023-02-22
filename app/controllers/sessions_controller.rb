1607887f58bbf8adc092287bd9df4f1ef707e6c4
Ubuntu <ubuntu@ip-172-31-32-179.ap-northeast-1.compute.internal>
2023-02-22 06:27:42 +0000

    log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  #delete /logout
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ validates :email,  presence: true,length: { maximum: 100 }, format: { with: VALI
 has_secure_password
 
 validates :password, presence: true, length: { minimum: 6 }
+
+ # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
